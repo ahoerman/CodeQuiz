d0cb935a50db11f9cdf39ed2539a219bc6a39901
A Hoerman <awhoerman@me.com>
2020-01-26 18:28:11 -0500

    added questions and timer penalty

--- script.js
+++ script.js
@@ -2,15 +2,53 @@ let timer = $("#timer");
 let timestart = 99;
 
 function quizTimer() {
-  setTimeout(function () {
-      if (timestart > -1) {
-        timer.text(timestart);
-      timestart--;
-      quizTimer();
-    }
-  }, 1000)
+    setTimeout(function () {
+        if (timestart > -1) {
+            timer.text(timestart);
+            timestart--;
+            quizTimer();
+        }
+    }, 1000)
 };
 
+function question1() {
+    $("#question").text("Is the sky blue?").attr("data-value", "a");
+    let answers = ["yes", "no", "no", "no"];
+    $("#a").text(answers[0]);
+    $("#b").text(answers[1]);
+    $("#c").text(answers[2]);
+    $("#d").text(answers[3]);
+}
+
+function question2() {
+    $("#question").text("Is water wet?").attr("data-value", "b");
+    let answers = ["no", "yes", "no", "no"];
+    $("#a").text(answers[0]);
+    $("#b").text(answers[1]);
+    $("#c").text(answers[2]);
+    $("#d").text(answers[3]);
+}
+
+function answerChoice(event) {
+    let penalty = 10
+    let correct = $("#question").attr("data-value");
+    let target = $(event.target);
+    if (target.is("li")) {
+        if (target.attr("data-value") === correct) {
+            question2();
+        }
+        else {
+            timestart = timestart - penalty;
+            question2();
+        }
+    }
+}
+
+
+$(".answerBtn").on("click", answerChoice);
 $("#startBtn").on("click", quizTimer);
+$("#startBtn").on("click", question1);
+
+
 
 
